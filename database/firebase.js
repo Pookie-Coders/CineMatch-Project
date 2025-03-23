b0767843cce5ce987b081e174703b066f574fc2e
unknown <abhishek2004karthik@gmail.com>
2025-03-23 07:41:38 +0530

    adding folers for document, database

---
+++ database/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHpbk6I4DnEBYnIbWOI4rjyQHsnfKvDVU",
+    authDomain: "cinematch-11e28.firebaseapp.com",
+    projectId: "cinematch-11e28",
+    storageBucket: "cinematch-11e28.firebasestorage.app",
+    messagingSenderId: "90273923749",
+    appId: "1:90273923749:web:2bb93b9add2c288687834c"
+  };
